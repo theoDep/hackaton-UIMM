8d589d79fb52607e040e5a55e4be11ae67b3de89
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2023-03-07 16:14:59 +0100

    Basic setup for ReactRouterDom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/routes" element={<App />} />
+        <Route path="/profile" element={<App />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
